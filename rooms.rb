5e1aa7b5890eccddfd5b09f01834f4ace3ed055f
Ross Hill <rosshill88@gmail.com>
2017-12-01 15:59:30 +0000

    mvp

--- rooms.rb
+++ rooms.rb
@@ -1,11 +1,34 @@
 class Rooms
 
-attr_reader :room_name
+attr_reader :room_name, :room_capacity
 
-  def initialize (room_name)
+  def initialize (room_name, room_capacity)
     @room_name = room_name
+    @room_capacity = room_capacity
+    @guests_in_room = []
+    @songs_in_room = []
     # @room_capacity = room_capacity
   end
 
+  def guest_count
+    @guests_in_room.length()
+  end
 
+  def add_guest_to_room(guest_name)
+    @guests_in_room << guest_name
+  end
+
+  def remove_guest_from_room(guest_name)
+    @guests_in_room.delete(guest_name)
+  end
+
+  def song_count
+    @songs_in_room.length()
+  end
+
+  def add_song_to_room(song)
+    @songs_in_room << song
+  end
+
+  
 end
--- specs/rooms_spec.rb
+++ specs/rooms_spec.rb
@@ -7,11 +7,41 @@ require_relative('../songs.rb')
 class RoomsTest < MiniTest::Test
 
   def setup
-    @room = Rooms.new("The Party Room")
+    @room_1 = Rooms.new("The Party Room", 6)
+    @room_2 = Rooms.new("The Disco Room", 11)
+    @guest_1 = Guests.new("Ross")
+    @guest_2 = Guests.new("Michael")
+    @guest_3 = Guests.new("Graeme")
+    @song_1 = Songs.new("Robot Stop")
+    @song_2 = Songs.new("Big Fig Wasp")
+    @song_3 = Songs.new("Gamma Knife")
   end
 
   def test_room_has_name
-    assert_equal("The Party Room", @room.room_name)
+    assert_equal("The Party Room", @room_1.room_name())
+  end
+
+  def test_room_is_empty
+    assert_equal(0, @room_1.guest_count())
+  end
+
+  def test_room_has_a_guest
+    @room_1.add_guest_to_room(@guest_1)
+    assert_equal(1, @room_1.guest_count)
+  end
+
+  def test_guest_can_be_removed_from_room
+    @room_1.add_guest_to_room(@guest_1)
+    @room_1.add_guest_to_room(@guest_2)
+    @room_1.add_guest_to_room(@guest_3)
+    @room_1.remove_guest_from_room(@guest_1)
+    assert_equal(2, @room_1.guest_count)
+  end
+
+  def test_song_can_be_added_to_room
+    @room_1.add_song_to_room(@song_1)
+    @room_1.add_song_to_room(@song_2)
+    assert_equal(2, @room_1.song_count)
   end
 
 end
